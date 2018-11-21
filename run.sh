b68e6998bd85efdaea99bf463402ece524096dff
Michal Trybulec <michal.trybulec@alertlogic.com>
2018-11-21 11:11:13 +0000

    Save/restore terminal state in run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+
+# Change terminal mode to send characters to standard input as soon as type -
+# don't wait for the Enter at the end of the line.
+# https://stackoverflow.com/questions/42750491/read-a-character-input-from-erlang-without-requiring-the-return-key-pressed-from
+
+save_tty_state=$(stty -g)
 stty -f /dev/tty icanon raw
 erl -pa ./src/ -run game play -run init stop -noshell
-stty echo echok icanon -raw
+stty "$save_tty_state"
